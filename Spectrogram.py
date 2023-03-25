786e0a5ee73f6621d9e5b3e5dc9b653bc7cc95f8
Dylan <dylancvdean@gmail.com>
2023-03-24 18:01:36 -0600

    zoom

--- Spectrogram.py
+++ Spectrogram.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedLocator, FixedFormatter
 
 dt = 0.0005
-t = np.arange(0.0, 20.0, dt)
+t = np.arange(0.0, 4.0, dt)  # 4000ms of signal
 s1 = np.sin(2 * np.pi * 100 * t)
 s2 = 2 * np.sin(2 * np.pi * 400 * t)
 s3 = np.sin(2 * np.pi * 400 * t)
@@ -32,7 +33,7 @@ def update_colormap(ax, colormap):
     fig.canvas.draw_idle()
 
 fig = plt.figure(figsize=(16, 9))
-gs = fig.add_gridspec(3, 3, width_ratios=(2, 8, 1), height_ratios=(4, 1, 1),
+gs = fig.add_gridspec(2, 3, width_ratios=(2, 8, 1), height_ratios=(4, 1),
                       left=0.1, right=0.9, bottom=0.1, top=0.9,
                       wspace=0.05, hspace=0.05)
 
@@ -41,16 +42,10 @@ ax_histx = fig.add_subplot(gs[1, 1], sharex=ax)
 ax_histy = fig.add_subplot(gs[0, 0])
 ax_colormap = fig.add_subplot(gs[0, 2])
 
-ax_time_labels = fig.add_subplot(gs[2, 1], sharex=ax_histx)
-ax_time_labels.tick_params(axis="x", labelbottom=True)
-ax_time_labels.tick_params(axis="y", labelleft=False, left=False)
-ax_time_labels.yaxis.set_label_position("right")
-ax_time_labels.set_ylabel("Time (ms)")
-
-ax_histx.tick_params(axis="x", labelbottom=False)
+ax.tick_params(axis="x", labelbottom=False)
 ax_histy.tick_params(axis="y", labelleft=True)
 
-Pxx, freqs, bins, im = ax.specgram(x1, NFFT=NFFT, Fs=Fs, noverlap=900, cmap=colormaps[current_colormap_idx])
+Pxx, freqs, bins, im = ax.specgram(x1, NFFT=NFFT, Fs=Fs, noverlap=0, cmap=colormaps[current_colormap_idx])
 
 fft = np.fft.fft(x1)
 freq = np.fft.fftfreq(len(x1), dt)
@@ -66,15 +61,12 @@ ax_histx.plot(t, x1)
 
 plt.colorbar(im, cax=ax_colormap)
 
-ax.set_xlim(0, 20)
-ax_histx.set_xlim(0, 20)
+ax.set_xlim(0, 1)  # 1000ms of signal
+ax_histx.set_xlim(0, 1)  # 1000ms of signal
+ax.set_xlabel('')  # Remove x-axis label from the spectrogram plot
+ax_histx.set_xlabel('Time (ms)')  # Add x-axis label to the time domain plot
 
 zoom_scale = 1.1
-
-def update_time_labels():
-    current_xlim = ax_histx.get_xlim()
-    ax_time_labels.set_xticklabels([f"{(t - current_xlim[1]) * 1000:.0f}" for t in ax_time_labels.get_xticks()])
-    fig.canvas.draw_idle()
 
 def on_key(event):
     global current_colormap_idx
@@ -95,6 +87,7 @@ def on_key(event):
         new_yrange = current_yrange / zoom_scale
     elif event.key == 'e':  # Zoom out on the frequency axis
         new_yrange = current_yrange * zoom_scale
+        current_ylim = (current_ylim[0], current_ylim[1] * zoom_scale)
     elif event.key == 'right':  # Cycle to the next colormap
         current_colormap_idx = (current_colormap_idx + 1) % len(colormaps)
         update_colormap(ax, colormaps[current_colormap_idx])
@@ -106,8 +99,8 @@ def on_key(event):
     else:
         return
 
-    if new_xrange > 20:
-        new_xrange = 20
+    if new_xrange > 1:  # 1000ms of signal
+        new_xrange = 1  # 1000ms of signal
 
     if new_yrange > max_freq:
         new_yrange = max_freq
@@ -119,26 +112,30 @@ def on_key(event):
     ycenter = np.mean(current_ylim)
 
     new_xlim = (xcenter - new_xrange / 2, xcenter + new_xrange / 2)
-    new_ylim = (ycenter - new_yrange / 2, ycenter + new_yrange / 2)
+    new_ylim = (current_ylim[0], ycenter + new_yrange / 2)
 
     ax.set_xlim(new_xlim)
     ax_histx.set_xlim(new_xlim)
 
     ax.set_ylim(new_ylim)
 
+    # Update the ylim for ax_histy subplot and rescale the amplitude data
     freq_mask = (freq >= new_ylim[0]) & (freq <= new_ylim[1])
     visible_freq = freq[freq_mask]
     visible_amp = amp[freq_mask]
-    visible_amp = visible_amp / np.max(visible_amp) * new_yrange / 2
+    visible_amp = visible_amp / np.max(amp) * new_yrange / 2
 
     ax_histy.clear()
     ax_histy.plot(visible_amp, visible_freq)
     ax_histy.set_ylim(new_ylim)
     ax_histy.set_ylabel('Frequency (Hz)')
 
-    update_time_labels()
+    # Update time axis labels
+    ax_histx.xaxis.set_major_locator(FixedLocator(ax_histx.get_xticks()))
+    ax_histx.xaxis.set_major_formatter(FixedFormatter([f"{x * 1000:.0f}" for x in ax_histx.get_xticks()]))
+
+    fig.canvas.draw_idle()
 
 fig.canvas.mpl_connect('key_press_event', on_key)
 
-update_time_labels()
 plt.show()
