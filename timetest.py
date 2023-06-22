f4aff31b9ed6a46ad49904d4e74d48d9fd069ae8
Dylan <dylancvdean@gmail.com>
2023-06-21 20:56:29 -0600

    time works

--- timetest.py
+++ timetest.py
@@ -8,19 +8,16 @@ dt = 0.001  # Set the time step to 1 ms
 t = np.arange(0.0, 1.0, dt)  # Set the time range to 0 to 1000 ms
 
 # Create a function to generate the signal at a given time
-def generate_signal(t, current_time):
+def generate_signal(t):
     signal_freq = 100
-    toggle_period = 0.4  # Period of toggling on and off in seconds
+    toggle_period = 0.2  # Period of toggling on and off in seconds
     sine_wave = 20 * np.sin(2.0 * np.pi * signal_freq * t)
     
     # Generate a square wave with values of 1 (on) and 0 (off) based on the toggle_period
     square_wave = 20 * np.array([int((ti % (2 * toggle_period)) < toggle_period) for ti in t])
     #print(square_wave)
     toggled_sine_wave = sine_wave * square_wave
-    print(sine_wave)
-    print("break")
     return toggled_sine_wave
-    return sine_wave
 
 os.makedirs('./spec', exist_ok=True)
 
@@ -34,12 +31,13 @@ fft_buffer = np.zeros((100, len(t)), dtype=np.complex128)
 while True:
     # Generate the signal for the current time window
     current_t = np.linspace(current_time, current_time + 1.0, 40*int(1.0/dt), endpoint=False)
-    x1 = generate_signal(current_t, current_time)
-
-    #x2=1/40
-    x2 = np.array(x1[::40])
+    current_t2 = np.linspace(current_time+0.22, current_time+0.22 + 1.0, 40*int(1.0/dt), endpoint=False)
+    x1 = generate_signal(current_t)
     x1=x1[0:1000]
-    print(x2)
+    #x2=1/40
+    x2 = generate_signal(current_t2)
+    x2 = np.array(x2[::40])
+    
     # Calculate the FFT of the signal
     fft_data = np.fft.fft(x1)
 
